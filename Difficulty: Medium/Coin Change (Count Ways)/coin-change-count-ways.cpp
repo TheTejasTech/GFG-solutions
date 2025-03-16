f928118565b4846fa33d493b258962424f7d2a76
Tejas Kamble <kambletc@gmail.com>
2025-03-16 12:46:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int count(vector<int>& coins, int sum) {
+        // code here.
+        vector<int> dp (sum +1, 0);
+        int n = coins.size(); 
+        dp[0] = 1 ; 
+        for(int i=0 ; i<n ; i++){
+            for(int coin = coins[i] ; coin<=sum ; coin++) 
+                dp[coin] += dp[ coin - coins[i]];
+        }
+        return dp[sum];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
+        Solution ob;
+        cout << ob.count(arr, sum) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
