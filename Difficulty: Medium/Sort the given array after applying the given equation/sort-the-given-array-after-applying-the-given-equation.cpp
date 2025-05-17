04c6205307581d84118dee37d006b055ad256403
Tejas Kamble <kambletc@gmail.com>
2025-05-17 12:16:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort the given array after applying the given equation/sort-the-given-array-after-applying-the-given-equation.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> sortArray(vector<int> &arr, int A, int B, int C) {
+        // code here
+        int n=arr.size();
+        vector<int> sol(n,0);
+        for(int i=0;i<n;i++){
+            sol[i]=(A*(arr[i]*arr[i]))+(B*arr[i])+C;
+        }
+        sort(sol.begin(),sol.end());
+        return sol;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int a, b, c;
+        cin >> a >> b >> c;
+        cin.ignore();
+
+        Solution obj;
+        vector<int> ans = obj.sortArray(arr, a, b, c);
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << ' ';
+        cout << endl;
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
